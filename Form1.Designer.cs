5d5e4aca9bbb638ca42fb9bc36bbe78e6a29ca43
hk-rigacoding <hk.rigacoding@gmail.com>
2021-06-02 21:59:54 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace database
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.log = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.query = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // log
+            // 
+            this.log.Location = new System.Drawing.Point(12, 114);
+            this.log.Multiline = true;
+            this.log.Name = "log";
+            this.log.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.log.Size = new System.Drawing.Size(649, 340);
+            this.log.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(535, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(126, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Send Query";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // query
+            // 
+            this.query.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.query.Location = new System.Drawing.Point(21, 53);
+            this.query.Name = "query";
+            this.query.ReadOnly = true;
+            this.query.Size = new System.Drawing.Size(508, 30);
+            this.query.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 466);
+            this.Controls.Add(this.query);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.log);
+            this.Name = "Form1";
+            this.Text = "DB";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox log;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox query;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace database
+{
+    public partial class Form1 : Form
+    {
+        //mūsu mysql servera pieslēguma dati
+        //string conn_str = "server=localhost;user=root;database=test;port=3306;password=jaguar";
+        //string conn_str = "server=localhost;user=pulkstenis;database=test;port=3306;password=parole";
+        string conn_str = "server=localhost;user=pulkstenis;port=3306;password=parole";
+
+        //mysql klienta instance
+        MySqlConnection conn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //ja nav instancēts, instancējam
+            if (conn == null)
+                conn = new MySqlConnection(conn_str);
+
+
+            //ja query ir tukšs, tad atveram konekciju
+            if (query.Text == "")
+            {
+                try
+                {
+                    log.Text = "Opening Connection to DB Server ... " + Environment.NewLine + log.Text;
+                    conn.Open();
+                    log.Text = "SUCCESS !" + Environment.NewLine + log.Text;
+                    query.ReadOnly = false;
+
+                }
+                catch (Exception exc)
+                {
+                    log.Text = exc.Message + Environment.NewLine + log.Text;
+                }
+            } else // aizpildīts query
+            {
+                try
+                {
+                    MySqlCommand cmd = new MySqlCommand(query.Text, conn);
+
+
+                    //if (query.Text.ToLower().Contains("select"))
+                    if (true)
+                    {
+                        //gadījums, kad izgūstam datus no datubāzes
+                        //cmd.ExecuteReader
+
+                        //veidu, kā izgūt datus
+                        MySqlDataReader reader = cmd.ExecuteReader();//sūtam query.text saturu komandu
+
+                        while (reader.Read())
+                        {
+                            string temp2 = "";
+
+                            for (int i=0; i< reader.FieldCount; i++)
+                                temp2 += reader[i].ToString();
+
+                            log.Text = temp2 + Environment.NewLine + log.Text;
+                        }
+
+                        reader.Close();
+
+                    } else
+                    {
+                        //gadījums, kad mums nav atbildes datu
+                        cmd.ExecuteNonQuery();
+
+
+                    }
+
+
+
+                }
+                catch (Exception exc)
+                {
+                    log.Text = exc.Message + Environment.NewLine + log.Text;
+                }
+
+            }
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            conn.Close();
+        }
+    }
+}
